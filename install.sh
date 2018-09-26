d69906e26633e13af04d4901e1eb2b96ed26d72c
Cristobal Dominguez <cristobald@gmail.com>
2018-09-26 01:04:55 -0300

    Automatizes the Installation With a Bash File

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Comenzando!"
+
+echo "Instalando wp-cli"
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+
+cp ./bash_php ${HOME}/.bash_php
+cp ./bash_profile ${HOME}/.bash_profile
+cp ./bash_prompt ${HOME}/.bash_prompt
+cp ./bashrc ${HOME}/.bashrc
+cp ./gemrc ${HOME}/.gemrc
+cp ./gitconfig ${HOME}/.gitconfig
+cp ./inputrc ${HOME}/.inputrc
+cp ./profile ${HOME}/.profile
+cp ./rubocop ${HOME}/.rubocop
+cp ./shortcuts ${HOME}/.shortcuts
+cp ./Brewfile ${HOME}/Brewfile
+
+echo "Instalando Packages de Brew"
+brew bundle
+rm ${HOME}/Brewfile
+
+echo "Listo!"
